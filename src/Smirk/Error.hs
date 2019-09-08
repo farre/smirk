94a0cfc1a17a5e9382aec9e90d77a184ef8eed60
Andreas Farre <farre@mozilla.com>
2019-09-08 16:03:22 +0200

    Added Smirk.Error and Smirk.Interpreter

---
+++ src/Smirk/Error.hs
@@ -0,0 +1,6 @@
+{-# Language FlexibleInstances #-}
+{-# Language MultiParamTypeClasses #-}
+{-# Language UndecidableInstances #-}
+module Smirk.Error where
+
+data Failure = Failure
---
+++ src/Smirk/Interpreter.hs
@@ -0,0 +1,4 @@
+module Smirk.Interpreter where
+
+import Smirk.Compiler
+import Smirk.Context
